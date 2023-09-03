d735bf85f7f1ebe574217955cb277ec18eece0c0
Gabriel Messias da Rosa <gabrielmessiasdarosa@gmail.com>
2023-09-02 21:30:24 -0300

    START: setup project.

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# magic-flea-market
+## MAGIC-FLEA-MARKET
+
+## Description
+
+Magic flea market is a market place where users can buy sell and trade magic cards. Users can create an account and post cards for sale. Users can also search for cards by name and filter by color, name and price. Users can also add cards to their cart and checkout. Users can also view their order history. Users can also view their profile and edit their profile. Users can also view other users profiles and see what cards they have for sale. Users can also have a want list of cards they are looking for. Users can also view their want list and remove cards from their want list. Users can also view other users want list and see what cards they are looking for. Users can also view their order history and see what cards they have purchased. Users can also view other users order history and see what cards they have purchased.
+
+Simple way to buy, sell, and trade magic cards.
+
+buy
+1 - Create an account
+2 - Search for cards
+3 - Contact seller
+4 - Negotiate price
+5 - Combine shipping
+
+sell
+1 - Create an account
+2 - Post cards for sale
+3 - Wait for buyer to contact you
+4 - Negotiate price
+5 - Combine shipping
+
+trade
+1 - Create an account
+2 - Post cards for trade
+3 - Wait for buyer to contact you
+4 - Negotiate price and trade conditions
+5 - Combine shipping
+
+For shipping we recommend using UBER motoboy.
---
+++ src/components/next-ui-exports.tsx
@@ -0,0 +1,129 @@
+"use client";
+
+import { Accordion, AccordionItem } from "@nextui-org/accordion";
+import { Avatar, AvatarGroup, AvatarIcon } from "@nextui-org/avatar";
+import { Badge } from "@nextui-org/badge";
+import { Button, ButtonGroup } from "@nextui-org/button";
+import { Card, CardBody, CardFooter, CardHeader } from "@nextui-org/card";
+import { Checkbox, CheckboxGroup } from "@nextui-org/checkbox";
+import { Chip } from "@nextui-org/chip";
+import { Code } from "@nextui-org/code";
+import { Divider } from "@nextui-org/divider";
+import {
+  Dropdown,
+  DropdownItem,
+  DropdownMenu,
+  DropdownSection,
+  DropdownTrigger,
+} from "@nextui-org/dropdown";
+import { Image } from "@nextui-org/image";
+import { Input, Textarea } from "@nextui-org/input";
+import { Kbd } from "@nextui-org/kbd";
+import { Link } from "@nextui-org/link";
+import {
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+} from "@nextui-org/modal";
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  NavbarMenu,
+  NavbarMenuItem,
+  NavbarMenuToggle,
+} from "@nextui-org/navbar";
+import {
+  Pagination,
+  PaginationCursor,
+  PaginationItem,
+} from "@nextui-org/pagination";
+import { Popover, PopoverContent, PopoverTrigger } from "@nextui-org/popover";
+import { CircularProgress, Progress } from "@nextui-org/progress";
+import { Radio, RadioGroup } from "@nextui-org/radio";
+import { Skeleton } from "@nextui-org/skeleton";
+import { Snippet } from "@nextui-org/snippet";
+import { Spacer } from "@nextui-org/spacer";
+import { Spinner } from "@nextui-org/spinner";
+import { Switch } from "@nextui-org/switch";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableColumn,
+  TableHeader,
+  TableRow,
+} from "@nextui-org/table";
+import { Tab, Tabs } from "@nextui-org/tabs";
+import { Tooltip } from "@nextui-org/tooltip";
+import { User } from "@nextui-org/user";
+
+export {
+  Accordion,
+  AccordionItem,
+  Avatar,
+  AvatarGroup,
+  AvatarIcon,
+  Badge,
+  Button,
+  ButtonGroup,
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  Checkbox,
+  CheckboxGroup,
+  Chip,
+  CircularProgress,
+  Code,
+  Divider,
+  Dropdown,
+  DropdownItem,
+  DropdownMenu,
+  DropdownSection,
+  DropdownTrigger,
+  Image,
+  Input,
+  Kbd,
+  Link,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  NavbarMenu,
+  NavbarMenuItem,
+  NavbarMenuToggle,
+  Pagination,
+  PaginationCursor,
+  PaginationItem,
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+  Progress,
+  Radio,
+  RadioGroup,
+  Skeleton,
+  Snippet,
+  Spacer,
+  Spinner,
+  Switch,
+  Tab,
+  Table,
+  TableBody,
+  TableCell,
+  TableColumn,
+  TableHeader,
+  TableRow,
+  Tabs,
+  Textarea,
+  Tooltip,
+  User,
+};
