c9da1016f06dac5037055ae427a055933635282c
Gabriel Messias da Rosa <gabrielmessiasdarosa@gmail.com>
2023-09-12 20:44:23 -0300

    feat: save progress

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,5 +1,7 @@
+import { mobileNavbarMenuStore } from "@/store/mobile-navbar-menu-store";
 import { Toaster } from "react-hot-toast";
 import MfmBrandLogo from "./mfm-brand-logo";
+import MobileNavbarMenu from "./mobile-navbar-menu";
 import NavbarItemsList from "./navbar-items";
 import { Button, Navbar as NUINavbar, NavbarContent } from "./next-ui-exports";
 import SearchCollapsableInput from "./search-collapsable-input";
@@ -7,8 +9,12 @@ import UserDropdownMenu from "./user-dropdown-menu";
 export interface NavbarProps {}
 
 export default function Navbar({}: NavbarProps) {
+  const { isOpen, setIsOpen } = mobileNavbarMenuStore();
   return (
     <NUINavbar
+      isBordered
+      isMenuOpen={isOpen}
+      onMenuOpenChange={setIsOpen}
       isBlurred
       height={"100px"}
       shouldHideOnScroll
@@ -16,7 +22,7 @@ export default function Navbar({}: NavbarProps) {
       className="h-36 shadow-sm lg:h-24"
     >
       <div className="flex h-fit  w-full flex-col items-center justify-between lg:flex-row">
-        <div className="debug flex w-full items-center">
+        <div className="flex w-full items-center">
           <NavbarContent
             justify="start"
             className="flex flex-col items-center md:flex-row"
@@ -47,10 +53,10 @@ export default function Navbar({}: NavbarProps) {
               <NavbarContent justify="center" className="hidden lg:flex">
                 <UserDropdownMenu />
               </NavbarContent>
-              <NavbarContent
-                justify="center"
-                className="flex lg:hidden"
-              ></NavbarContent>
+
+              <NavbarContent justify="center" className="flex lg:hidden">
+                <MobileNavbarMenu />
+              </NavbarContent>
             </div>
           </NavbarContent>
         </div>
